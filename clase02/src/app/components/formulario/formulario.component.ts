6b819145e7e0702e85b7f5b8069086c5b54ef42c
NAFerrero <nicolasferreroutn@gmail.com>
2019-09-06 18:48:48 -0300

    comentario

--- clase02/src/app/components/formulario/formulario.component.ts
+++ clase02/src/app/components/formulario/formulario.component.ts
@@ -4,26 +4,26 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent implements OnInit {
-
   @Output() cargar = new EventEmitter<any>();
   @Input() estado: any;
 
   persona: any = {
     nombre: '',
-    email: ''
+    email: '',
   };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   enviar(persona: any, event: Event) {
     event.preventDefault();
-    this.cargar.emit(new Persona(this.persona.nombre, this.persona.email)); // ESTO SE HACE PARA QUE NO QUEDEN TODOS REFERENCIADOS A LO MISMO, EXTRAE LAS PROPIEDADES Y CREA OTRO OBJETO
+    /* ESTO SE HACE PARA QUE NO QUEDEN TODOS REFERENCIADOS A LO MISMO, EXTRAE LAS PROPIEDADES Y CREA OTRO OBJETO
+    this.cargar.emit({...persona})*/
+    this.cargar.emit(new Persona(this.persona.nombre, this.persona.email));
     this.persona.nombre = '';
     this.persona.email = '';
   }

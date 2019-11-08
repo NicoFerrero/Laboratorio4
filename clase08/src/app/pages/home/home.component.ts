62995a94fc0b1509502e6ef5a3ea0c44940d7825
NAFerrero <nicolasferreroutn@gmail.com>
2019-11-08 16:50:03 -0300

    correcciones

--- clase08/src/app/pages/home/home.component.ts
+++ clase08/src/app/pages/home/home.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/auth/usuario.service';
 import { Auto } from 'src/app/models/auto';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import { UserOptions } from 'jspdf-autotable';
+
+interface jsPDFWithPlugin extends jsPDF {
+  autoTable: (options: UserOptions) => jsPDF;
+}
 
 @Component({
   selector: 'app-home',
@@ -51,5 +58,27 @@ export class HomeComponent implements OnInit {
     this.usuarioService.subirArchivo(event.target.files[0], path).then(data => {
       console.log('exito');
     });
+  }
+
+  getDataPDF(autos: Auto[]) {
+    const body = [];
+    for (let i = 0; i < this.autos.length; i++) {
+      body.push([
+        this.autos[i]['_id'],
+        this.autos[i]['marca'],
+        this.autos[i]['modelo'],
+        this.autos[i]['anio'],
+      ]);
+    }
+    return body;
+  }
+
+  descargarPDF() {
+    const doc = new jsPDF('portrait', 'px', 'a4') as jsPDFWithPlugin;
+    doc.autoTable({
+      head: [['id', 'marca', 'modelo', 'anio']],
+      body: this.getDataPDF(this.autos),
+    });
+    doc.save('grilla_autos');
   }
 }
